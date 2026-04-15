926dba0b66c9b58c1a3b21d8dcdcba7dad1d750b
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2026-04-15 17:59:46 -0300

    wip add players, scores, refactor for start button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GameBoard = (function () {
+const GameBoard = () => {
   let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   const resetBoard = () => {
@@ -7,8 +7,10 @@ const GameBoard = (function () {
     }
   };
 
-  return { board, resetBoard };
-})();
+  const getBoard = () => board;
+
+  return { resetBoard, getBoard };
+};
 
 const TurnsCounter = (function () {
   let counter = 0;
@@ -56,16 +58,16 @@ const GameRules = (function () {
 })();
 
 const GameController = (function () {
-  const { board, resetBoard } = GameBoard;
   const { resetCounter } = TurnsCounter;
+  const startGameBtn = document.querySelector("#start-game-btn");
   let symbol = "X";
   let winner;
   let isGameOver = false;
+  const gameBoard = GameBoard();
 
+  const getBoard = gameBoard.getBoard();
   const getWinner = () => winner;
-
   const getIsGameOver = () => isGameOver;
-
   const getSymbol = () => symbol;
 
   const changeSymbol = () => {
@@ -76,15 +78,21 @@ const GameController = (function () {
   const fillTile = (tilePosition) => {
     if (isGameOver) return;
 
-    if (board[tilePosition] === "X" || board[tilePosition] === "O") {
+    if (
+      gameBoard.getBoard()[tilePosition] === "X" ||
+      gameBoard.getBoard()[tilePosition] === "O"
+    ) {
       console.log("invalid move");
       return false;
     }
 
-    board[tilePosition] = getSymbol();
+    gameBoard.getBoard()[tilePosition] = getSymbol();
     changeSymbol();
 
-    winner = GameRules.verifyVictory(board, TurnsCounter.getCounter());
+    winner = GameRules.verifyVictory(
+      gameBoard.getBoard(),
+      TurnsCounter.getCounter(),
+    );
     TurnsCounter.addToCounter();
 
     if (winner === "X" || winner === "O" || winner === "tie") {
@@ -93,28 +101,44 @@ const GameController = (function () {
     return true;
   };
 
+  const startGame = (playersList) => {};
+
   const gameRestart = () => {
-    resetBoard();
+    gameBoard.resetBoard();
     resetCounter();
     symbol = "X";
     isGameOver = false;
     winner = null;
   };
 
-  return { fillTile, gameRestart, getIsGameOver, getWinner };
+  return {
+    fillTile,
+    gameRestart,
+    getIsGameOver,
+    getWinner,
+    getBoard,
+    startGame,
+  };
 })();
 
 const DisplayController = (function () {
-  const { board } = GameBoard;
-  const { fillTile, gameRestart, getIsGameOver, getWinner } = GameController;
+  const {
+    fillTile,
+    gameRestart,
+    getIsGameOver,
+    getWinner,
+    getBoard,
+    startGame,
+  } = GameController;
   const gameTable = document.querySelector("#game-table");
   const gameContainer = document.querySelector("#game-container");
+  const winnerText = document.querySelector("#winner-text");
 
   const handleTileClick = (element, index) => {
     const isValidMove = fillTile(Number(index));
 
     if (isValidMove) {
-      element.innerText = board[index];
+      element.innerText = getBoard[index];
 
       if (getIsGameOver()) {
         displayWinner();
@@ -123,7 +147,6 @@ const DisplayController = (function () {
   };
 
   const displayWinner = () => {
-    const winnerText = document.querySelector("#winner-text");
     winnerText.setAttribute("id", "winner-text");
 
     if (getWinner() === "tie") {
@@ -133,14 +156,48 @@ const DisplayController = (function () {
     }
   };
 
-  board.forEach((tile, index) => {
-    const gameTile = document.createElement("div");
-    gameTile.setAttribute("class", "game-tile");
+  const displayBoard = (board) => {
+    board.forEach((tile, index) => {
+      const gameTile = document.createElement("div");
+      gameTile.setAttribute("class", "game-tile");
 
-    gameTile.addEventListener("click", () => handleTileClick(gameTile, index));
+      gameTile.addEventListener("click", () =>
+        handleTileClick(gameTile, index),
+      );
 
-    gameTable.append(gameTile);
-  });
+      gameTable.append(gameTile);
+    });
+  };
+
+  const grabPlayersNames = () => {
+    const players = document.querySelectorAll(".players");
+    const playersList = Array.from(players).map(({ value }) => value);
+    return playersList;
+  };
+
+  const displayStartBtn = () => {
+    const startBtn = document.createElement("button");
+    startBtn.setAttribute("id", "start-game-btn");
+    startBtn.innerText = "Start";
+    startBtn.addEventListener("click", () => {
+      displayBoard(getBoard);
+      startGame(grabPlayersNames());
+    });
+    gameContainer.insertBefore(startBtn, winnerText);
+  };
+  displayStartBtn();
+
+  const displayPlayer = (playerOne, playerTwo) => {
+    const spanPlayerOne = document.createElement("span");
+    spanPlayerOne.innerText = playerOne;
+    spanPlayerOne.setAttribute("class", "players");
+    gameContainer.insertBefore(spanPlayerOne, winnerText);
+
+    const spanPlayerTwo = document.createElement("span");
+    spanPlayerTwo.innerText = playerTwo;
+    spanPlayerTwo.setAttribute("class", "players");
+    gameContainer.insertBefore(spanPlayerTwo, winnerText);
+  };
 
   const restartBtn = document.createElement("button");
   restartBtn.innerText = "↩ Restart";
@@ -159,6 +216,14 @@ const DisplayController = (function () {
       findWinnerText.innerText = "";
     }
   });
+
+  return { displayBoard, displayPlayer, grabPlayersNames };
 })();
 
-console.log(GameBoard.board);
+const Player = (name) => {
+  const playerName = name;
+
+  const getPlayerName = () => playerName;
+
+  return { getPlayerName };
+};
